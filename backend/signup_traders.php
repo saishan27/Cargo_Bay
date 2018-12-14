8356b244183328e2ad97f09f6e31fc2fb88d049e
SaiDharshan <saidharshanshan@gmail.com>
2018-12-14 23:11:54 +0530

    changes indirectory
    Co-Authored-By: Jaya1601 <jaya1601@users.noreply.github.com>
    Co-Authored-By: DeeviyaM <deeviyam@users.noreply.github.com>
    Co-Authored-By: VinushaS <45868263+vinushas@users.noreply.github.com>

--- backend/signup_traders.php
+++ backend/signup_traders.php
@@ -1,1 +1,39 @@
-<?php	
+<?php 
+		
+	date_default_timezone_set('Asia/Kolkata');
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$dbname = "Cargo_Bay";
+	// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		
+	$fname = $_POST["fname"];
+	$lname = $_POST["lname"];
+	$mail = $_POST["mail"];
+	$uname = $_POST["uname"];
+	$passwd = $_POST["password"]
+	$mob = $_POST["phone"]
+	
+		$sql = "INSERT INTO traders (username,firstname,lastname,temail,mobile,password) 
+		VALUES ('".$uname."','".$fname."','".$lname."',
+		'".$mail."','".$mob."','".$passwd."')"; 
+	
+		if ($conn->query($sql) === TRUE) {
+			echo "Traders Account signup ccessfull";
+		} else {
+			echo "Error Creating Account: " . $conn->error;
+		}
+		
+		$conn->close();
+		
+?>
+	
+	
+			
+				
+				

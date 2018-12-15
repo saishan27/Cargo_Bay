72fea9856f1e9506d2cf0f21a6b21cdd3437148b
SaiDharshan <saidharshanshan@gmail.com>
2018-12-15 10:52:33 +0530

    linking interface with backend

--- team_work/singing.php
+++ team_work/singing.php
@@ -1,5 +1,5 @@
 <?php
-    $servername = "localhost";
+   $servername = "localhost";
 	$username = "root";
 	$password = "root";
 	$dbname = "Cargo_Bay";
@@ -15,42 +15,42 @@
       $myusername = mysqli_real_escape_string($db,$_POST['uname']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']);
       $utype = $_POST["TYPE"]; 
-      
+     
       if($utype == "Traders"){
-
+         
       $sql = "SELECT username FROM traders WHERE username = '$myusername' and password = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
+      //$active = $row['active'];
       
       $count = mysqli_num_rows($result);
       
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
-         session_register("myusername");
+      
          $_SESSION['login_user'] = $myusername;
-         
-         header("location: ../team_work/find.php");
+         echo "logged in";
+         header("location: Find.php");
       }else {
          $error = "Your Login Name or Password is invalid";
       }
-   } else if($utype == Shippers){
+   } if($utype == "Shippers"){
 
       $sql = "SELECT provider_id FROM ser_provider WHERE provider_name = '$myusername' and provider_password = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
+      //$active = $row['active'];
       
       $count = mysqli_num_rows($result);
       
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
          
-         header("location: ../team_work/Offer.php");
+         $_SESSION['login_user'] = $myusername;
+         echo "logged in";
+         header("location: Offer.php");
       }else {
          $error = "Your Login Name or Password is invalid";
       }
